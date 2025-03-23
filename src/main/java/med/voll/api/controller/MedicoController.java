e6044ca51fff3f22da0e1a7ddb91e0aadaddd90d
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2025-03-23 20:52:38 -0300

    V4.3

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,10 +6,10 @@ import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -25,7 +25,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DatosListaMedico> listar() {
-        return repository.findAll().stream().map(DatosListaMedico::new).toList();
+    public Page<DatosListaMedico> listar(Pageable paginacion) {
+        return repository.findAll(paginacion).map(DatosListaMedico::new);
     }
 }
